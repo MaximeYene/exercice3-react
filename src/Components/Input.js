5ea12bf0ceda3deb75173fbca1a9eb1ca844d2b9
Maxime Yene <maximeyene@gmail.com>
2023-09-26 14:55:20 +0100

    fix some errors

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -7,19 +7,26 @@ function Formulaire(){
 
 const [inputValue,setInputValue]=useState("");
 const [list,setList]=useState([]);
-const handleClick=()=>{setList((previewList)=>[...previewList,inputValue]);
+const handleClick=(e)=>{
+    e.preventDefault();
+    setList((previewList)=>[...previewList,inputValue]);
 setInputValue("");
 }
 
    return (
     <div>
         <h1 className="title">Exercice3 React</h1>
+        <p className="title-explication">Le but de cet exercice est de créer avec React une action qui lorsqu'on clique sur le bouton <strong>Afficher</strong> renvoie la valeur de l'input dans la div à coté.</p>
     <div className="container">
         <div className="container-form">
-            <input type="text" value={inputValue} onChange={(event)=>setInputValue(event.target.value)}/><br/>
-            <button onClick={handleClick}>Affichez</button>
+            <form onSubmit={handleClick}>
+            <input type="text" value={inputValue} onChange={(event)=>setInputValue(event.target.value)} required/><br/>
+            <button >Ajouter</button>
+            </form>
+           
         </div>
         <div className="container-output">
+            <p>Listage des donnees :</p>
             <ul>
                 {list.map((mot)=>(
                     <li>{mot}</li>
